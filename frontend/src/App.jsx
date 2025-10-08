1201e0a6c6d9c4cee598af44d18cf0f592174364
Aryan Das <aryandas2911@gmail.com>
2025-10-08 10:59:47 +0530

    npm modules imported

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
     <div>
-      <h1 className='text-3xl font-bold underline'> Hi</h1>
+      
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
