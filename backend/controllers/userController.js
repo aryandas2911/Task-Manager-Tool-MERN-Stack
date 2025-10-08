d4b7f26fb79a9da8bb2fd6d82b025bef21960ebf
Aryan Das <aryandas2911@gmail.com>
2025-10-08 16:04:24 +0530

    Register feature added

---
+++ backend/controllers/userController.js
@@ -0,0 +1,52 @@
+import User from "../models/userModel.js";
+import validator from "validator";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET || "your_jet_secret_here";
+const TOKEN_EXPIRES = "24h";
+
+const createToken = (userID) => {
+  jwt.sign({ id: userID }, JWT_SECRET, { expiresIn: TOKEN_EXPIRES });
+};
+
+//Register user
+export async function registerUser(req, res) {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    return res
+      .status(400)
+      .json({ success: false, message: "All fields required" });
+  }
+  if (!validator.isEmail(email)) {
+    return res.status(400).json({ success: false, message: "Invalid email" });
+  }
+  if (password.length < 8) {
+    return res.status(400).json({
+      success: false,
+      message: "Password must be atleast 8 characters",
+    });
+  }
+
+  try {
+    if (await User.findOne({ email })) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User already exists" });
+    }
+
+    const hashed = await bcrypt.hash(password, 10);
+    const user = await User.create({ name, email, password: hashed });
+    const token = createToken(user._id);
+
+    res.status(201).json({
+      success: true,
+      token,
+      user: { id: user._id, name: user.name, email: user.email },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+}
