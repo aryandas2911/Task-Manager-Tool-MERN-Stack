c84584c02b9190f527a204c15b89f1eebcd9801a
Aryan Das <aryandas2911@gmail.com>
2025-10-11 02:16:41 +0530

    Task schema created

---
+++ backend/models/taskModel.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+//Task Schema
+const taskSchema = new mongoose.Schema({
+  task: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    default: "",
+  },
+  priority: {
+    type: String,
+    enum: ["Low", "Medium", "High"],
+    default: "Low",
+  },
+  dueDate: {
+    type: Date,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+//Task model
+const taskModel = mongoose.model("task", taskSchema);
+
+export default taskModel;
