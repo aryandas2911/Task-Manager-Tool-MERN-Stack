6516ce61a88179d3d208b3019583a5905e6aad51
Aryan Das <aryandas2911@gmail.com>
2025-10-11 03:03:14 +0530

    Tasks routes created and initialized

---
+++ backend/routes/taskRoute.js
@@ -0,0 +1,24 @@
+import express from "express";
+import authMiddleware from "../middlewares/auth";
+import {
+  createTask,
+  deleteTask,
+  getTaskByID,
+  getTasks,
+  updateTask,
+} from "../controllers/taskController";
+
+const taskRouter = express.Router();
+
+taskRouter
+  .route("/gp")
+  .get(authMiddleware, getTasks)
+  .post(authMiddleware, createTask);
+
+taskRouter
+  .route("/:id/gp")
+  .get(authMiddleware, getTaskByID)
+  .post(authMiddleware, updateTask)
+  .delete(authMiddleware, deleteTask);
+
+export default taskRouter;
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import { connectDB } from "./config/db.js";
 import userRouter from "./routes/userRoute.js";
+import taskRouter from "./routes/taskRoute.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -17,6 +18,7 @@ connectDB();
 
 //Routes
 app.use("/api/user",userRouter)
+app.use("/api/tasks",taskRouter)
 
 app.get("/", (req, res) => {
   res.send("API Working");
